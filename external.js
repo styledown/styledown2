36d11c0e141abf0390e5a86678e7d43d8e6b5741
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-10 00:06:56 +0800

    Refactor the way bin writes

--- external.js
+++ external.js
@@ -1,4 +1,5 @@
 // Export for Ruby
 exports.parse = require('./lib/parse')
 exports.render = require('./lib/render')
+exports.build = require('./lib/build')
 exports.version = require('./package.json').version

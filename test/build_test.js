4b5ce9863ae69cc488acbbeb323ffa19f495e442
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-10 13:51:35 +0800

    Fix anchor links

--- test/build_test.js
+++ test/build_test.js
@@ -2,7 +2,7 @@ var test = require('ava')
 var styledown = require('../index')
 var r = require('redent')
 
-test('rendering TOC', t => {
+test('one link item', t => {
   var out = styledown.parse([
     { name: 'README.md',
       data: r(`
@@ -16,7 +16,117 @@ test('rendering TOC', t => {
   var data = styledown.build(out)
   var html = data.files['components.html'].sections.menu
 
-  t.regex(html, /<ul class="styleguide-menu">/)
-  t.regex(html, /<li class="styleguide-menu-item -level-1">/)
-  t.regex(html, /<a href="components.html"/)
+  t.deepEqual(html, [
+    '<ul class="styleguide-menu">',
+    '<li class="styleguide-menu-item -level-1">',
+    '<a href="components.html" class="link title">Components</a>',
+    '</li>',
+    '</ul>'
+  ].join(''))
+})
+
+test('one text item', t => {
+  var out = styledown.parse([
+    { name: 'README.md',
+      data: r(`
+        # Table of Contents
+        * Components
+      `) },
+    { name: 'components.md',
+      data: 'Hello' }
+  ])
+
+  var data = styledown.build(out)
+  var html = data.files['components.html'].sections.menu
+
+  t.deepEqual(html, [
+    '<ul class="styleguide-menu">',
+    '<li class="styleguide-menu-item -level-1">',
+    '<span class="title">Components</span>',
+    '</li>',
+    '</ul>'
+  ].join(''))
+})
+
+test('one link and one text item', t => {
+  var out = styledown.parse([
+    { name: 'README.md',
+      data: r(`
+        # Table of Contents
+        * Buttons
+        * [Components](components.md)
+      `) },
+    { name: 'components.md',
+      data: 'Hello' }
+  ])
+
+  var data = styledown.build(out)
+  var html = data.files['components.html'].sections.menu
+
+  t.deepEqual(html, [
+    '<ul class="styleguide-menu">',
+    '<li class="styleguide-menu-item -level-1">',
+    '<span class="title">Buttons</span>',
+    '</li>',
+    '<li class="styleguide-menu-item -level-1">',
+    '<a href="components.html" class="link title">Components</a>',
+    '</li>',
+    '</ul>'
+  ].join(''))
+})
+
+test('subitems', t => {
+  var out = styledown.parse([
+    { name: 'README.md',
+      data: r(`
+        # Table of Contents
+        * [Components](components.md)
+          * Buttons
+      `) },
+    { name: 'components.md',
+      data: 'Hello' }
+  ])
+
+  var data = styledown.build(out)
+  var html = data.files['components.html'].sections.menu
+
+  t.deepEqual(html, [
+    '<ul class="styleguide-menu">',
+    '<li class="styleguide-menu-item -level-1 -parent">',
+    '<a href="components.html" class="link title">Components</a>',
+    '<ul class="submenu">',
+    '<li class="styleguide-menu-item -level-2">',
+    '<span class="title">Buttons</span>',
+    '</li>',
+    '</ul>',
+    '</li>',
+    '</ul>'
+  ].join(''))
+})
+
+test('anchor', t => {
+  var out = styledown.parse([
+    { name: 'README.md',
+      data: r(`
+        # Table of Contents
+        * [Components](components.md)
+        * [Other components](components.md#hi)
+      `) },
+    { name: 'components.md',
+      data: 'Hello' }
+  ])
+
+  var data = styledown.build(out)
+  var html = data.files['components.html'].sections.menu
+
+  t.deepEqual(html, [
+    '<ul class="styleguide-menu">',
+    '<li class="styleguide-menu-item -level-1">',
+    '<a href="components.html" class="link title">Components</a>',
+    '</li>',
+    '<li class="styleguide-menu-item -level-1">',
+    '<a href="components.html#hi" class="link title">Other components</a>',
+    '</li>',
+    '</ul>'
+  ].join(''))
 })

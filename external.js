023f18ba9cb953f90cac20fbec819ea4c9533fef
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-11 15:30:19 +0800

    Move test files around

--- external.js
+++ external.js
@@ -2,4 +2,5 @@
 exports.parse = require('./lib/parse')
 exports.render = require('./lib/render')
 exports.build = require('./lib/build')
+exports.templates = require('./lib/templates')
 exports.version = require('./package.json').version

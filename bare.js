17e800fa7bb53c1b2deb16010afa9153366304c2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-12 12:03:52 +0800

    Move stuff away for deprecation

--- bare.js
+++ bare.js
@@ -1,6 +1,6 @@
 // Export for Ruby
 exports.parse = require('./lib/parse')
 exports.render = require('./lib/render')
-exports.build = require('./lib/build')
+// exports.build = require('./lib/build')
 exports.templates = require('./lib/templates')
 exports.version = require('./package.json').version

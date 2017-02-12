d29ca7077028891285ab7eb9b1d0fd63e8d27d33
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-13 01:31:28 +0800

    Implement --skip-assets

--- test/build/index.test.js
+++ test/build/index.test.js
@@ -2,21 +2,23 @@ const test = require('ava')
 const build = require('../../lib/build')
 const dedent = require('dedent')
 
+const FILES = {
+  'components.md': {
+    contents: dedent `
+      # Components
+      ### header
+      This is a header
+
+      ~~~ example.haml
+      = render 'header'
+      ~~~`
+  }
+}
+
 test('block with example', t => {
-  var out = build({
-    'components.md': {
-      contents: dedent `
-        # Components
-        ### header
-        This is a header
+  const result = build(FILES)
 
-        ~~~ example.haml
-        = render 'header'
-        ~~~`
-    }
-  })
-
-  var file = out.files['components.md']
+  var file = result.files['components.md']
   t.true(file.title === 'Components')
 
   var header = file.sections.header
@@ -30,4 +32,21 @@ test('block with example', t => {
   t.true(header.parts.s2.type === 'example')
   t.true(header.parts.s2.language === 'haml')
   t.regex(header.parts.s2.content, /= render 'header'/)
+})
+
+test('renders assets', t => {
+  const result = build(FILES)
+
+  t.true(result.files['styledown/script.js'].type === 'application/javascript')
+  t.true(result.files['styledown/style.css'].type === 'text/css')
+
+  t.true(result.files['styledown/script.js'].layout === 'script')
+  t.true(result.files['styledown/style.css'].layout === 'style')
+})
+
+test('respects skipAssets: true', t => {
+  const result = build(FILES, { skipAssets: true })
+
+  t.true(typeof result.files['styledown/script.js'] === 'undefined')
+  t.true(typeof result.files['styledown/style.css'] === 'undefined')
 })

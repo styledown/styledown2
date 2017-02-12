b26f509d0e97ac9727c3dc4d82fa38e506371ced
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-12 12:56:38 +0800

    Add more tests

--- test/build/transform_lang.test.js
+++ test/build/transform_lang.test.js
@@ -15,3 +15,32 @@ test('transformLang: works', t => {
   t.is(result.sections[0].parts[0].source, 'a.btn Hello')
   t.is(result.sections[0].parts[0].content, '<a class="btn">Hello</a>')
 })
+
+test('transformLang: leaves others alone', t => {
+  const result = transformLang({
+    sections: [
+      { parts: [
+        { type: 'example', language: 'haml', content: '%a.btn Hello' }
+      ] }
+    ]
+  }, { transform: ['jade'] })
+
+  t.is(result.sections[0].parts[0].language, 'haml')
+  t.is(result.sections[0].parts[0].sourceLanguage, undefined)
+  t.is(result.sections[0].parts[0].content, '%a.btn Hello')
+})
+
+test('transformLang: works with pug', t => {
+  const result = transformLang({
+    sections: [
+      { parts: [
+        { type: 'example', language: 'pug', content: 'a.btn Hello' }
+      ] }
+    ]
+  }, { transform: ['pug'] })
+
+  t.is(result.sections[0].parts[0].language, 'html')
+  t.is(result.sections[0].parts[0].sourceLanguage, 'pug')
+  t.is(result.sections[0].parts[0].source, 'a.btn Hello')
+  t.is(result.sections[0].parts[0].content, '<a class="btn">Hello</a>')
+})

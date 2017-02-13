03b8143aa1542726624deb2164f53a1f342d008f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-02-13 20:19:18 +0800

    Support mime types in render

--- test/render/index.test.js
+++ test/render/index.test.js
@@ -4,7 +4,7 @@ const render = require('../../lib/render')
 test('works', t => {
   const data = {
     files: {
-      'buttons.html': { name: 'John', layout: 'html' }
+      'buttons.html': { name: 'John', layout: 'html', type: 'text/html' }
     },
     templates: {
       'html': 'Hello {{name}}'
@@ -14,8 +14,34 @@ test('works', t => {
   const result = render(data)
 
   t.deepEqual(Object.keys(result), ['buttons.html'])
-  t.deepEqual(Object.keys(result['buttons.html']), ['contents'])
+  t.deepEqual(Object.keys(result['buttons.html']), ['contents', 'type'])
   t.deepEqual(result['buttons.html'].contents, 'Hello John')
+  t.deepEqual(result['buttons.html'].type, 'text/html')
+})
+
+test('works without type', t => {
+  const data = {
+    files: {
+      'buttons.html': { name: 'John', layout: 'html' }
+    },
+    templates: {
+      'html': 'Hello {{name}}'
+    }
+  }
+
+  const result = render(data)
+  t.deepEqual(result['buttons.html'].type, undefined)
+})
+
+test('works without layout', t => {
+  const data = {
+    files: {
+      'buttons.html': { name: 'John' }
+    }
+  }
+
+  const result = render(data)
+  t.deepEqual(result['buttons.html'], { contents: '', type: undefined })
 })
 
 test('accounts for meta', t => {
@@ -30,7 +56,7 @@ test('accounts for meta', t => {
   const result = render(data)
 
   t.deepEqual(Object.keys(result), ['buttons.html'])
-  t.deepEqual(Object.keys(result['buttons.html']), ['contents'])
+  t.deepEqual(Object.keys(result['buttons.html']), ['contents', 'type'])
   t.deepEqual(result['buttons.html'].contents, 'Hello John')
 })
 
